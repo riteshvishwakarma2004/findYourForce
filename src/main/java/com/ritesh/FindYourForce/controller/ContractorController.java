cbf1cccd437fe86af7f572e8c67307f768eccc1d
Ritesh Ramesh Vishwakarma <vishwakarmaritesh253@gmail.com>
2025-04-14 14:51:12 +0530

    added home

--- src/main/java/com/ritesh/FindYourForce/controller/ContractorController.java
+++ src/main/java/com/ritesh/FindYourForce/controller/ContractorController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("Tradesperson")
+//@RequestMapping("Tradesperson")
 public class ContractorController {
 
     @Autowired
